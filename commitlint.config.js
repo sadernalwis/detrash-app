c97bc555561add5f96c02dd7245b150acf7b44f3
Yuri Mutti <muttiyuri@gmail.com>
2024-04-17 17:29:54 +0200

    feat: commit lint

--- commitlint.config.js
+++ commitlint.config.js
@@ -1,80 +1,27 @@
+// build: Changes that affect the build system or external dependencies (example scopes: gulp, broccoli, npm)
+// ci: Changes to our CI configuration files and scripts (example scopes: Travis, Circle, BrowserStack, SauceLabs)
+// docs: Documentation only changes
+// feat: A new feature
+// fix: A bug fix
+// perf: A code change that improves performance
+// refactor: A code change that neither fixes a bug nor adds a feature
+// style: Changes that do not affect the meaning of the code (white-space, formatting, missing semi-colons, etc)
+// test: Adding missing tests or correcting existing tests
+
 const Configuration = {
-
-  /*
-   * Whether commitlint uses the default ignore rules, see the description above.
-   */
-  defaultIgnores: true,
-
-
-
-  /*
-     * Resolve and load @commitlint/config-conventional from node_modules.
-     * Referenced packages must be installed
-     */
   extends: ['@commitlint/config-conventional'],
-
-
-
-
-  /*
-     * Resolve and load @commitlint/format from node_modules.
-     * Referenced package must be installed
-     */
-  formatter: '@commitlint/format',
-
-
-
-
-
-  /*
-     * Custom URL to show upon failure
-     */
-  helpUrl:
-    'https://github.com/conventional-changelog/commitlint/#what-is-commitlint',
-
-
-
-
-
-  /*
-     * Array of functions that return true if commitlint should ignore the given message.
-     * Given array is merged with predefined functions, which consist of matchers like:
-     *
-     * - 'Merge pull request', 'Merge X into Y' or 'Merge branch X'
-     * - 'Revert X'
-     * - 'v1.2.3' (ie semver matcher)
-     * - 'Automatic merge X' or 'Auto-merged X into Y'
-     *
-     * To see full list, check https://github.com/conventional-changelog/commitlint/blob/master/%40commitlint/is-ignored/src/defaults.ts.
-     * To disable those ignores and run rules always, set `defaultIgnores: false` as shown below.
-     */
-  ignores: [(commit) => commit === ''],
-
-
-
-  /*
-     * Resolve and load conventional-changelog-atom from node_modules.
-     * Referenced packages must be installed
-     */
-  parserPreset: 'conventional-changelog-atom',
-
-
-  /*
-     * Custom prompt configs
-     */
-  prompt: {
-    messages: {},
-    questions: {
-      type: {
-        description: 'please input type:',
-      },
-    },
-  },
-
-  /*
-   * Any rules defined here will override rules from @commitlint/config-conventional
-   */
   rules: {
+    'body-leading-blank': [1, 'always'],
+    'body-max-line-length': [2, 'always', 100],
+    'footer-leading-blank': [1, 'always'],
+    'footer-max-line-length': [2, 'always', 100],
+    'header-max-length': [2, 'always', 100],
+    'scope-case': [2, 'always', 'lower-case'],
+    'subject-case': [2, 'never', ['sentence-case', 'start-case', 'pascal-case', 'upper-case']],
+    'subject-empty': [2, 'never'],
+    'subject-full-stop': [2, 'never', '.'],
+    'type-case': [2, 'always', 'lower-case'],
+    'type-empty': [2, 'never'],
     'type-enum': [
       2,
       'always',
