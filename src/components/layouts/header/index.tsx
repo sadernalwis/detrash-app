e13548c2117fbd9e856ea37520a855cd7e72b5d9
Yuri Mutti <muttiyuri@gmail.com>
2024-04-26 19:45:02 +0200

    refactor: header sticky

--- src/components/layouts/header/index.tsx
+++ src/components/layouts/header/index.tsx
@@ -2,8 +2,10 @@ import { Menu } from './menu';
 
 export const Header = () => {
   return (
-    <header className="mx-auto my-5 flex items-center justify-between sm:container">
-      <Menu />
+    <header className="sticky top-0 mx-auto mb-8 flex w-full items-center justify-between border-b bg-white p-4">
+      <div className="container">
+        <Menu />
+      </div>
     </header>
   );
 };
